0b17379ff71fb5823a6fa00b05bbae7ef7a61969
juryheed <rabitangl20@naver.com>
2024-05-19 00:58:11 +0900

    init String boot

---
+++ src/main/java/org/mjulikelion/likelionmessengerservice/LikelionMessengerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.likelionmessengerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionMessengerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionMessengerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mjulikelion/likelionmessengerservice/LikelionMessengerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.likelionmessengerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionMessengerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
